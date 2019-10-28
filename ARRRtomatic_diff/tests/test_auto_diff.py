51c7247de10ebfc7295846a11b96c69e66945336
victor <osiajod@gmail.com>
2019-10-28 18:54:53 -0400

    dummy test

--- ARRRtomatic_diff/tests/test_auto_diff.py
+++ ARRRtomatic_diff/tests/test_auto_diff.py
@@ -11,9 +11,11 @@
 #     assert (1+x)['val'] ==3
 #     assert (1+x)['d_x'] ==1
 import pytest
-# from ARRRtomatic_diff.auto_diff import AutoDiff
+# from ARRRtomatic_diff.functions import funcs
+
 
 def test_add():
+    # from ARRRtomatic_diff.auto_diff import AutoDiff
     # x = AutoDiff(name='x', val=2)
     # y = AutoDiff(name='y', val=-5)
     # print(type(x+1))
