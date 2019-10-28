7c2384ebd2159e881859aa3c23363c6481ac526f
victor <osiajod@gmail.com>
2019-10-27 22:20:21 -0400

    dummy test

--- ARRRtomatic_diff/test_auto_diff.py
+++ ARRRtomatic_diff/test_auto_diff.py
@@ -7,8 +7,9 @@ y = AutoDiff(name='y', val=-5)
 def test_add():
      
     assert (x+1)['val'] ==3
-    assert (x+1)['val'] ==3
+    assert (x+1)['d_x'] ==1
     assert (1+x)['val'] ==3
+    assert (1+x)['d_x'] ==1
 
 
 
