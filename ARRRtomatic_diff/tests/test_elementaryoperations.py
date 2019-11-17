97059c5c7ede1895e3690a07d49672c7391d538c
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-11-17 13:29:18 -0500

    corrected addition assert error

--- ARRRtomatic_diff/tests/test_elementaryoperations.py
+++ ARRRtomatic_diff/tests/test_elementaryoperations.py
@@ -6,7 +6,7 @@ def test_addition():
     y = AutoDiff(name='a1', val=0)
     z = AutoDiff(name='a2', val=-200)
 
-    assert x + x == 3, 'Addition failed'
+    assert x + x == 6, 'Addition failed'
     assert y + y == 0, 'Addition failed'
     assert z + z == -400, 'Addition failed'
 
