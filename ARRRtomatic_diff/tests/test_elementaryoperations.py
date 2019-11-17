42bc8a3f826e5b8f92d0dd912fee2824595b449f
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-11-17 13:26:26 -0500

    modified test for elementary operations

--- ARRRtomatic_diff/tests/test_elementaryoperations.py
+++ ARRRtomatic_diff/tests/test_elementaryoperations.py
@@ -1,19 +1,21 @@
 
+from ARRRtomatic_diff import AutoDiff
+
 def test_addition():
-    x = AutoDiffVariable(name='a0', val=3)
-    y = AutoDiffVariable(name='a1', val=0)
-    z = AutoDiffVariable(name='a2', val=-200)
+    x = AutoDiff(name='a0', val=3)
+    y = AutoDiff(name='a1', val=0)
+    z = AutoDiff(name='a2', val=-200)
 
     assert x + x == 3, 'Addition failed'
     assert y + y == 0, 'Addition failed'
     assert z + z == -400, 'Addition failed'
 
     # addition
-    print("Addition")
-    print(1 + x)
-    print(x + 1)
-
-    print(x + x)
-
-    print(x + y)
-    print(y + x)
+    # print("Addition")
+    # print(1 + x)
+    # print(x + 1)
+    #
+    # print(x + x)
+    #
+    # print(x + y)
+    # print(y + x)
