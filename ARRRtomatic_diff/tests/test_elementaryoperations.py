0d7c22c973ff73f3886539a06c48947a6edaf3bd
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-11-17 14:28:18 -0500

    added subtract and mul tests

--- ARRRtomatic_diff/tests/test_elementaryoperations.py
+++ ARRRtomatic_diff/tests/test_elementaryoperations.py
@@ -16,7 +16,35 @@ def test_add():
    assert (z + z).trace['val'] == 0, 'Addition failed'
    assert (z + z).trace['d_z'] == 2, 'Addition failed'
 
+def test_subtract():
+   # from ARRRtomatic_diff.auto_diff import AutoDiff
+   x = AutoDiff(name='x', val=9)
+   y = AutoDiff(name='y', val=-5)
+   z = AutoDiff(name='z', val=0)
+   assert (x - 1).trace['val'] == 8, 'Subtraction failed'
+   assert (x - 1).trace['d_x'] == 1, 'Subtraction failed'
+   assert (1 - x).trace['val'] == -8, 'Subtraction failed'
+   assert (1 - x).trace['d_x'] == 1, 'Subtraction failed'
+   assert (y - x).trace['val'] == -14, 'Subtraction failed'
+   assert (y - x).trace['d_x'] == 1, 'Subtraction failed'
+   assert (y - x).trace['d_y'] == 1, 'Subtraction failed'
+   assert (z + z).trace['val'] == 0, 'Subtraction failed'
+   assert (z + z).trace['d_z'] == 2, 'Subtraction failed'
 
+def test_multiply():
+   # from ARRRtomatic_diff.auto_diff import AutoDiff
+   x = AutoDiff(name='x', val=6)
+   y = AutoDiff(name='y', val=-5)
+   z = AutoDiff(name='z', val=0)
+   assert (x * 2).trace['val'] == 12, 'Multiplication failed'
+   assert (x * 1).trace['d_x'] == 2, 'Multiplication failed'
+   assert (2 * x).trace['val'] == 12, 'Multiplication failed'
+   assert (2 * x).trace['d_x'] == 2, 'Multiplication failed'
+   assert (y * x).trace['val'] == -30, 'Multiplication failed'
+   assert (y * x).trace['d_x'] == -5, 'Multiplication failed'
+   assert (y * x).trace['d_y'] == 6, 'Multiplication failed'
+   assert (z * z).trace['val'] == 0, 'Multiplication failed'
+   assert (z * z).trace['d_z'] == 0, 'Multiplication failed'
 
 # def test_add():
 #    # from ARRRtomatic_diff.auto_diff import AutoDiff
