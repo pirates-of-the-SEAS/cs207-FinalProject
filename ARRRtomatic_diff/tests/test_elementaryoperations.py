dfe1f550a38fc1ae31e8e8a57e87f99eba855909
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-11-17 13:35:56 -0500

    Improving additions test

--- ARRRtomatic_diff/tests/test_elementaryoperations.py
+++ ARRRtomatic_diff/tests/test_elementaryoperations.py
@@ -1,14 +1,24 @@
 
 from ARRRtomatic_diff import AutoDiff
 
-def test_addition():
-    x = AutoDiff(name='a0', val=3)
-    y = AutoDiff(name='a1', val=0)
-    z = AutoDiff(name='a2', val=-200)
+def test_add():
+   # from ARRRtomatic_diff.auto_diff import AutoDiff
+   x = AutoDiff(name='x', val=2)
+   y = AutoDiff(name='y', val=-5)
+   # print(type(x+1))
+   assert (x + 1).trace['val'] == 3
+   assert (x + 1).trace['d_x'] == 1
+   assert (1 + x).trace['val'] == 3
+   assert (1 + x).trace['d_x'] == 1
 
-    assert x + x == 6, 'Addition failed'
-    assert y + y == 0, 'Addition failed'
-    assert z + z == -400, 'Addition failed'
+# def test_addition():
+#     x = AutoDiff(name='a0', val=3)
+#     y = AutoDiff(name='a1', val=0)
+#     z = AutoDiff(name='a2', val=-200)
+#
+#     assert x + x == 6, 'Addition failed'
+#     assert y + y == 0, 'Addition failed'
+#     assert z + z == -400, 'Addition failed'
 
     # addition
     # print("Addition")
