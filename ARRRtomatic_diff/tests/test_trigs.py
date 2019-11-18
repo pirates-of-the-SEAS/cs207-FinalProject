4ad4e5035da1f26f49f9a60cb2addcbf0dcf085a
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-11-17 19:24:53 -0500

    updated trigs

--- ARRRtomatic_diff/tests/test_trigs.py
+++ ARRRtomatic_diff/tests/test_trigs.py
@@ -4,5 +4,4 @@ import numpy as np
 
 def test_cos():
     x = AutoDiff(name='x', val=np.pi)
-    # assert (np.cos(x.trace['val'])) == -1, "Old cosine failed"
-    assert (ad.cos(x)).trace['val'] == -1, "New cosine failed"
+    assert (ad.cos(x)) == -1, "New cosine failed"
