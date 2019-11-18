5367b0805b30d854dd723439778d66c068c26eb3
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-11-18 00:44:58 -0500

    finished first pass on trigs test

--- ARRRtomatic_diff/tests/test_trigs.py
+++ ARRRtomatic_diff/tests/test_trigs.py
@@ -22,42 +22,48 @@ def test_tan():
 
 def test_csc():
     x = AutoDiff(name='x', val=np.pi/2)
-    assert np.allclose(ad.csc(x).trace['val'], 1, atol=1e-12), "Cosecant failed"
-    # y = AutoDiff(name='y', val=np.pi)
-    # print(ad.csc(np.pi))
+    assert np.allclose(ad.csc(x).trace['val'], 1, atol=1e-12) is True, "Cosecant failed"
+    assert np.allclose(ad.csc(x).trace['d_x'], 0, atol=1e-12) is True, "Cosecant failed"
 
 def test_sec():
     x = AutoDiff(name='x', val=0)
     assert np.allclose(ad.sec(x).trace['val'], 1, atol=1e-12) is True, "Secant failed"
-    # assert np.allclose(ad.sec(x).trace['d_x'], 1, atol=1e-12) is True, "Secant failed"
+    assert np.allclose(ad.sec(x).trace['d_x'], 0, atol=1e-12) is True, "Secant failed"
 
-# def test_cot():
-    # x = AutoDiff(name='x', val=np.pi/4)
-    # assert np.allclose(ad.cot(x), 1, atol=1e-12), "Cotangent failed"
+def test_cot():
+    x = AutoDiff(name='x', val=np.pi/2)
+    assert np.allclose(ad.cot(x).trace['val'], 0, atol=1e-12), "Cotangent failed"
+    assert np.allclose(ad.cot(x).trace['d_x'], -1, atol=1e-12), "Cotangent failed"
 
 def test_asin():
     x = AutoDiff(name='x', val=0)
     assert np.allclose(ad.asin(x).trace['val'], 0, atol=1e-12) is True, "Arcsin failed"
+    assert np.allclose(ad.asin(x).trace['d_x'], 1, atol=1e-12) is True, "Arcsin failed"
 
 def test_acos():
     x = AutoDiff(name='x', val=0)
-    assert np.allclose(ad.arccos(x).trace['val'], np.pi/2, atol=1e-12) is True, 'Arccos failed'
+    assert np.allclose(ad.acos(x).trace['val'], np.pi/2, atol=1e-12) is True, 'Arccos failed'
+    assert np.allclose(ad.acos(x).trace['d_x'], -1, atol=1e-12) is True, "Arccos failed"
 
 def test_atan():
     x = AutoDiff(name='x', val=np.pi/2)
     assert np.allclose(ad.atan(x).trace['val'], 1.00388482185388721414842, atol=1e-12) is True, "Arctan failed"
+    assert np.allclose(ad.atan(x).trace['d_x'], 0.2884004391420009, atol=1e-12) is True, "Arctan failed"
 
 def test_acsc():
     x = AutoDiff(name='x', val=np.pi/2)
     assert np.allclose(ad.acsc(x).trace['val'], 0.69010709137453995200437, atol=1e-12) is True, "Arccosecant failed"
+    assert np.allclose(ad.acsc(x).trace['d_x'], -0.525539910519202993781, atol=1e-12) is True, "Arccosecant failed"
 
 def test_asec():
     x = AutoDiff(name='x', val=2)
     assert np.allclose(ad.asec(x).trace['val'], np.pi/3, atol=1e-12) is True, "Arcsecant failed"
+    assert np.allclose(ad.asec(x).trace['d_x'], 1/(2*np.sqrt(3)), atol=1e-12) is True, "Arcsecant failed"
 
 def test_acot():
     x = AutoDiff(name='x', val=1)
     assert np.allclose(ad.acot(x).trace['val'], np.pi/4, atol=1e-12) is True, "Arccotangent failed"
+    assert np.allclose(ad.acot(x).trace['d_x'], -0.5, atol=1e-12) is True, "Arccotangent failed"
 
 def test_sinh():
     x = AutoDiff(name='x', val=-1)
