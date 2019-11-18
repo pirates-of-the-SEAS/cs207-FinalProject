ba607790467192638ecf8bed5c461cce4c27f4bd
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-11-17 22:33:05 -0500

    uploading so I can test exp

--- ARRRtomatic_diff/tests/test_intermediateoperations.py
+++ ARRRtomatic_diff/tests/test_intermediateoperations.py
@@ -1,9 +1,30 @@
 from ARRRtomatic_diff import AutoDiff
 from ARRRtomatic_diff import functions as ad
 import numpy as np
+import warnings
 
-# def test_log():
-#     x = AutoDiff(name='x')
+def test_log():
+    x = AutoDiff(name='x', val=4)
+    y = AutoDiff(name='y', val=0)
+    z = AutoDiff(name='z', val=-2)
+    assert np.allclose((ad.log(x)).trace['val'], np.log(4), atol=1e-12) == True, 'Log failed'
+    assert ad.log(x).trace['d_x'] == 1/4, 'Log failed'
+    # try:
+    #     ad.log(y)
+    # except TypeError:
+    #     print("Caught TypeError as expected")
+
+    # with warnings.catch_warnings():
+    #     warnings.filterwarnings('error')
+    #     try:
+    #         ad.log(z).trace['val']
+    #     except Warning as e:
+    #         print('error found:', e)
+    # try:
+    #     ad.log(z)
+    # except RuntimeWarning:
+    #     print("Caught Warning as expected")
+
 #
 # def test_Euler():
 #
