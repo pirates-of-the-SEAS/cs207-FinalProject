2c57d8d2224f5ae9538a3122a534c0826e554c5a
Michael Downs <michaeldowns@Michaels-MacBook-Pro.local>
2019-11-18 15:31:30 -0500

    Added exceptions for nan unary operations

--- ARRRtomatic_diff/functions/funcs.py
+++ ARRRtomatic_diff/functions/funcs.py
@@ -28,10 +28,20 @@ def __update_unary(x, operation, doperation):
         updated_trace = {}
         updated_trace.update(trace)
 
-        updated_trace['val'] = operation(val)
+        updated_val = operation(val)
+        if np.isnan(updated_deriv):
+                raise ValueError
+
+        updated_trace['val'] = updated_val
+        
 
         for var in named_variables:
-            updated_trace[f'd_{var}'] = doperation(val) * updated_trace[f'd_{var}']
+            updated_deriv = doperation(val) * updated_trace[f'd_{var}']
+
+            if np.isnan(updated_deriv):
+                raise ValueError
+
+            updated_trace[f'd_{var}'] =  updated_deriv 
 
         return AutoDiff(name=named_variables,
                         trace=updated_trace)
