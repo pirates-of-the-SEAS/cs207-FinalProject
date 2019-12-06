f7229394144dc8c9fc1930c0d3cf223f2e6661b2
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-12-05 22:05:38 -0500

    changed data path for SGD

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -19,18 +19,33 @@ def test_gradientdescent():
 
 def test_example_loss():
     w0 = np.array([-1, 1])
-    df = pd.read_csv('../data/sgd_example.csv', header=None).T
+    df = pd.read_csv('./data/sgd_example.csv', header=None).T
     X = df.values
     target, lambdas = opt.example_loss(w0, X, None)
     assert np.allclose(target.val, 0.0004343472)
 
 # def test_sgd():
-#     df = pd.read_csv('../data/sgd_example.csv', header=None).T
 #     w0 = np.array([-1, 1])
+#     df = pd.read_csv('../data/sgd_example.csv', header=None).T
 #     X = df.values
-#     output = opt.do_stochastic_gradient_descent(w0, opt.rosenbrock, X, tol=1e-6,
+#     output = opt.do_stochastic_gradient_descent(w0, opt.example_loss, X, num_epochs=100, tol=1e-6,
 #                                                 use_momentum=True,
-#                                                 step_size=0.001)
-#     np.testing.assert_almost_equal(output, [1, 1], decimal=3), 'Gradient descent failed'
+#                                                 step_size=0.1)
+#     np.testing.assert_almost_equal(output, [2.054, 0.04], decimal=2), 'Stochastic gradient descent failed'
 
-# # def test_
+
+### IS THE DESCRIPTION OF THIS FUNCTION CORRECT IN THE optimization FOLDER????
+### SEEMS THAT F MAY NOT RETURN VALUE AND &&&& AND &&& DERIVATIVE ???
+def test_newtons_method_scalar():
+    result = opt.do_newtons_method(0, opt.example_scalar, tol=1e-8, verbose=0)
+    np.testing.assert_almost_equal(result, 0), "Newton's method for the scalar case failed"
+
+def test_newtons_method_multivariate():
+    w0 = np.array([1, 0])
+    result = opt.do_newtons_method(w0, opt.example_multivariate, tol=1e-8, verbose=0)
+    np.testing.assert_almost_equal(result, [10.7337749, -9.6865773]), "Newton's method for the vector case failed"
+
+def test_bfgs_scalar():
+    bfgs_output = opt.do_bfgs(0, opt.example_scalar, tol=1e-8, verbose=0)
+    print("RESULT", bfgs_output)
+    assert np.allclose(bfgs_output, -1.5707963), 'BFGS scalar failed'
