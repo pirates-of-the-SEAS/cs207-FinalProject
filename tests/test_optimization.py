486838b9c787f1274197ca3f16a78b794bf1ccae
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-12-05 19:53:59 -0500

    GD and Rosenbrock initial tests

---
+++ tests/test_optimization.py
@@ -0,0 +1,16 @@
+from ARRRtomatic_diff import AutoDiff, AutoDiffVector
+from ARRRtomatic_diff import optimization as opt
+import math
+import numpy as np
+
+def test_rosenbrock():
+    w = np.array([-1, 1])
+    ros, list = opt.rosenbrock(w)
+    np.testing.assert_array_equal(ros.val, 4,), 'Rosenbrock function failed'
+
+def test_gradientdescent():
+    w0 = np.array([-1, 1])
+    output = opt.do_gradient_descent(w0, opt.rosenbrock, max_iter=11000, step_size=0.001)
+    np.testing.assert_almost_equal(output, [1, 1], decimal=2), 'Gradient descent failed'
+    output = opt.do_gradient_descent(w0, opt.rosenbrock, use_momentum=True, max_iter=2000, step_size=0.001)
+    np.testing.assert_almost_equal(output, [1, 1], decimal=3), 'Gradient descent with momentum failed'
