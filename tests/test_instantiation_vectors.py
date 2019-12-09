1062ecea8bd0ddcacfa35599a788e78665459e01
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2019-12-09 01:32:37 -0500

    instantiaton vectors final tests

--- tests/test_instantiation_vectors.py
+++ tests/test_instantiation_vectors.py
@@ -12,28 +12,37 @@ def test_instantiation_pos():
     np.testing.assert_array_equal(J, [[1, 0], [0, 1]]), "Positive instantiation failed"
     np.testing.assert_almost_equal(v.val, [2, 2]), "Positive instantiation failed"
     J, order = v.get_jacobian()
-    # p.testing.assert_array_equal(J, [[1, 0], [0, 1]]), "Positive instantiation failed"
+    np.testing.assert_array_equal(J, [[0], [0]]), "Positive instantiation failed"
 
 
 def test_instantiation_neg():
     f1 = AutoDiff(name='x', val=-1)
     f2 = AutoDiff(name='y', val=-3)
     u = AutoDiffVector((f1, f2))
+    v = AutoDiffVector([-2, -2])
     np.testing.assert_array_equal(u.val, [-1,-3]), "Negative instantiation failed"
     J, order = u.get_jacobian()
     np.testing.assert_array_equal(J, [[1, 0], [0, 1]]), "Negative instantiation failed"
+    np.testing.assert_almost_equal(v.val, [-2, -2]), "Negative instantiation failed"
+    J, order = v.get_jacobian()
+    np.testing.assert_array_equal(J, [[0], [0]]), "Negative instantiation failed"
 
 
 def test_instantiation_zero():
-    x = AutoDiffVector(name='x', val=0)
-    assert x.trace['val'] == 0, 'Zero instantiation failed'
-    assert x.trace['d_x'] == 1, 'Zero instantiation failed'
+    f1 = AutoDiff(name='x', val=0)
+    f2 = AutoDiff(name='y', val=0)
+    u = AutoDiffVector((f1, f2))
+    np.testing.assert_array_equal(u.val, [0, 0]), "Positive instantiation failed"
+    J, order = u.get_jacobian()
+    np.testing.assert_array_equal(J, [[1, 0], [0, 1]]), "Positive instantiation failed"
+
 
 def test_instantiation_noname():
     try:
         AutoDiffVector(val=4)
-    except ValueError:
+    except TypeError:
         print("Caught error as expected")
+
 
 def test_bogus_instantiation():
     try:
@@ -45,18 +54,18 @@ def test_bogus_instantiation():
 def test_empty_instantiation():
     try:
         AutoDiffVector()
-    except ValueError:
+    except TypeError:
         print("Caught error as expected")
 
 
 def test_double_instantiation():
     try:
         AutoDiffVector(name='x', val=3, trace=3)
-    except ValueError:
+    except TypeError:
         print("Caught error as expected")
-
-def test_nameless_instantiation():
+    f1 = AutoDiff(name='x', val=1)
+    f2 = AutoDiff(name='y', val=3)
     try:
-        AutoDiffVector(val=3)
-    except ValueError:
+        AutoDiffVector((f1, f2), f1)
+    except TypeError:
         print("Caught error as expected")
